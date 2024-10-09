c308a67f3fb3ef75d2a35fd5cae8a9cd56b5ddc6
ntijoh-jesper-tordebring-jansson <jesper.tordebringjansson@elev.ga.ntig.se>
2024-10-09 14:58:54 +0200

    api token and back-end for search form.

---
+++ middleware/api.js
@@ -0,0 +1,39 @@
+const dotenv = require('dotenv')
+dotenv.config();
+
+class Api {
+    constructor() {
+        this.token = process.env.GITHUB_API_TOKEN;
+    }
+
+    async getRepository(name) {
+        this.url = `https://api.github.com/users/${name}/repos`;
+        this.repositories = [];
+
+        try {
+            const response = await fetch(this.url, {
+                headers: {
+                    "Authorization": `token ${this.token}`
+                }
+            });
+            
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+    
+            const json = await response.json();
+
+            for(let i = 0; i < json.length; i++) {
+                this.repositories.push(
+                    { "name" : json[i].name, "url" : json[i].html_url, "forks_url" : json[i].forks_url, "forks_count" : json[i].forks_count }
+                );
+            }
+
+            return this.repositories;
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+}
+
+module.exports = Api;
